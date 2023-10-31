466660c681ab40a9692199d23d7e157b99792c5d
Berdnaski <berdnaski1177@gmail.com>
2023-10-31 11:24:48 -0300

    responsive phone

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="flex justify-center items-center h-screen ">
         <div class="flex">
-            <div class="bg-gray-300 p-6 rounded-tl rounded-bl shadow-md w-[40rem] h-[50rem] justify-center flex flex-col items-center">
+            <div class="bg-gray-300 p-6 lg:rounded-tl lg:rounded-bl shadow-md lg:w-[40rem] lg:h-[50rem] w-[23rem] h-[35rem] justify-center flex flex-col items-center">
                 <div class="flex justify-center">
                     <h1 class="text-7xl font-bold flex justify-center">Sign In</h1>
                 </div>
@@ -16,14 +16,14 @@
                         <h3>use your account</h3>
                     </div>
 
-                    <div class="mb-4 w-[26rem]">
+                    <div class="mb-4 w-[18rem]">
                         <label for="email" class="text-sm">E-mail</label>
                         <input type="email" name="email" id="email" class="w-full border border-gray-300 p-2 rounded-md">
                     </div>
 
                     <div class="mb-4">
                         <label for="password" class="text-sm">Senha</label>
-                        <input type="password" name="password" id="password" class="w-full border border-gray-300 p-2 rounded-md">
+                        <input type="password" name="password" id="password" class="w-full border border-gray-900 p-2 rounded-md bg-transparent">
                     </div>
 
                     <div class="mb-4 flex justify-center gap-5">
@@ -45,7 +45,7 @@
                 </form>
             </div>
 
-            <div class="bg-red-500 p-6 rounded shadow-md  w-[40rem] h-[50rem]">
+            <div class="bg-red-500 p-6 rounded shadow-md  w-[40rem] h-[50rem] lg:block hidden">
                 <h1 class="text-red-600">shaushadasdasdauhsau</h1>
             </div>
         </div>
